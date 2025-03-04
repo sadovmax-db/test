1fa67d74c31889e305a3bd9558c79e95ec78195d
Sadovnichenko Maksym <m.sadovnichenko@dev-branch.com>
2025-03-04 22:44:27 +0200

    Add JavaScript and PHP coding standards validation workflows using GrumPHP

---
+++ web/themes/custom/some_theme/test.js
@@ -0,0 +1,20 @@
+// Missing semicolon
+console.log("Hello, World!")
+
+// Undefined variable usage
+let sum = a + b;
+
+// Incorrect function definition
+function testFunction 
+    console.log("This is a test function.")
+
+// Incorrect object syntax
+let obj = { key: "value", };
+
+// Using reserved keyword as variable name
+let function = "reserved";
+
+// Incorrect if statement syntax
+if sum > 10 {
+    console.log("Sum is greater than 10.");
+}
