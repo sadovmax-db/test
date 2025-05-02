5febe70245a11a5ede14c7033f402b8ff6d3bd41
Sadovnichenko Maksym <m.sadovnichenko@dev-branch.com>
2025-05-02 15:30:44 +0300

    Remove outdated PHP and JavaScript linter files; add new PHP standards checker workflow with GrumPHP integration.

--- web/themes/custom/some_theme/test.js
+++ web/themes/custom/some_theme/test.js
@@ -1,20 +1,85 @@
-// Missing semicolon
-console.log("Hello, World!")
+// test-linter.js
 
-// Undefined variable usage
-let sum = a + b;
+let name = "Test";
+const age = 30;
+const PI = 3.14;
 
-// Incorrect function definition
-function testFunction 
-    console.log("This is a test function.")
-
-// Incorrect object syntax
-let obj = { key: "value", };
-
-// Using reserved keyword as variable name
-let function = "reserved";
-
-// Incorrect if statement syntax
-if sum > 10 {
-    console.log("Sum is greater than 10.");
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayHello = function() {
+    console.log(`Hi my name is ${this.name}`);
+  };
 }
+
+const p = new Person(name, age);
+p.sayHello();
+
+// Function never used
+function unusedFunction(a, b, c, d, e, f, g) {
+  return a + b + c + d + e + f + g;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(`i is ${i}`);
+  for (let j = 0; j < 5; j++) {
+    console.log(`j is ${j}`);
+  }
+}
+
+const list = [1, 2, 3, 4, , 6, 7]; // double comma
+list.forEach(function(item, index) {
+  console.log(`Item at ${index} is ${item}`);
+});
+
+if ((name = "Other")) {
+  // assignment instead of comparison
+  console.log("Name matched");
+}
+
+function messyFunction(x, y) {
+  return x * y;
+}
+
+// Deprecated var use
+const obj = new Object();
+obj.name = "Object";
+obj.type = "generic";
+obj.say = function() {
+  console.log(`I am ${obj.name}`);
+};
+
+obj.say();
+
+function add(a, b) {
+  const result = a + b;
+  return resultt; // typo in return variable
+}
+
+// No semicolon, bad naming, magic numbers
+function doStuff() {
+  let result = 0;
+  for (let i = 0; i < 100; i++) {
+    result += i * 3.14159;
+  }
+  console.log("Done");
+}
+
+doStuff();
+
+// Uncaught exception not handled
+function risky() {
+  throw "This is bad";
+}
+
+risky();
+
+// Eval use – bad practice
+eval("console.log('EVAL IS BAD')");
+
+// trailing comma
+const data = {
+  a: 1,
+  b: 2,
+  c: 3
+};
--- web/themes/custom/some_theme/test.php
+++ web/themes/custom/some_theme/test.php
@@ -1,24 +1,88 @@
 <?php
 
-// Missing semicolon and incorrect echo usage
-echo "Hello, World!"
+/**
+ * @file
+ * Test-linter.php.
+ */
 
-// Undefined variable usage
-$sum = $a + $b;
+require_once 'somefile.php';
 
-// Incorrect function definition (missing opening brace)
-function testFunction() 
-    echo "This is a test function.";
+/**
+ *
+ */
+class testclass {
+  public $Var1 = "hello";
+  private $var2 = 123;
+  protected $VAR3;
 
-// Incorrect array usage
-$array = [1, 2, 3;
+  public function __construct() {
+    echo "Testclass constructed";
+    $this->Var1 = "Hi";
+  }
 
-// Using reserved keyword as variable name
-$function = "reserved";
+  /**
+   *
+   */
+  public function doSomething($param1, $param2) {
+    if ($param1 = $param2) {
+      echo "Params equal!";
+    }
+    else {
+      echo 'Params not equal!';
+    }
+  }
 
-// Incorrect if statement syntax
-if $sum > 10 {
-    echo "Sum is greater than 10.";
+  /**
+   *
+   */
+  public function unusedFunction() {
+    $temp = 100;
+  }
+
+  /**
+   *
+   */
+  public function calcSum($a, $b) {
+    $result = $a + $b;
+    // Undefined variable.
+    return $Result;
+  }
+
+  /**
+   *
+   */
+  public function veryLongFunctionNameThatDoesNotMakeSenseAndIsTooVerboseToBeUsefulOrReadable1234567890() {
+    for ($i = 0; $i < 10; $i++) {
+      echo $i;
+      for ($j = 0; $j < 5; $j++) {
+        echo $j;
+      }
+    }
+  }
+
 }
 
-?>
+// Missing semicolon.
+$test = new testclass()
+
+$test->doSomething(5, "5");
+
+/**
+ *
+ */
+function anotherFunc($arg1, $arg2, $arg3, $arg4, $arg5) {
+  echo "This function has too many arguments";
+}
+
+$arr = ["one", "two", "three"];
+foreach ($arr as $key => $value) {
+  echo $value
+}
+
+$someStr = "Hello";
+if ($someStr = "World") {
+  echo "Assigned instead of compared";
+}
+
+// Extra closing brace.
+}
